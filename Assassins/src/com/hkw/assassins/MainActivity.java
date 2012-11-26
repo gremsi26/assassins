59d483b0841d4224724e7adb56475fdbafabd8de
gremsi26 <gremsi26@gmail.com>
2012-11-25 19:01:23 -0500

    Added map features.

--- Assassins/src/com/hkw/assassins/MainActivity.java
+++ Assassins/src/com/hkw/assassins/MainActivity.java
@@ -1,12 +1,21 @@
 package com.hkw.assassins;
 
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Overlay;
 
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.DialogFragment;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -23,6 +32,8 @@ import android.widget.TextView;
 
 public class MainActivity extends MapActivity { 
 	
+	private MapView mapView;
+    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -31,8 +42,19 @@ public class MainActivity extends MapActivity {
         final ActionBar actionBar = getActionBar();
         actionBar.setDisplayShowTitleEnabled(true);
         
+        initMap();
     }
+    /**
+     * Initialise the map and adds the zoomcontrols to the LinearLayout.
+     */
+    private void initMap() {
+        mapView = (MapView) findViewById(R.id.mapView);
 
+        View zoomView = mapView.getZoomControls();
+        mapView.displayZoomControls(true);
+        mapView.getController().setZoom(17);
+    }
+    
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
     	
@@ -61,6 +83,14 @@ public class MainActivity extends MapActivity {
         	  targetinfoFragment.show(getFragmentManager(), "missiles");
         	 return true;
             case R.id.menu_settings:
+                return true;
+            case R.id.menu_share:
+            	Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            	sharingIntent.setType("text/plain");
+            	String shareBody = "Here is the share content body";
+            	sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+            	sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            	startActivity(Intent.createChooser(sharingIntent, "Share via"));
                 return true;
             case R.id.menu_startgame:
            	 DialogFragment startgameFragment = new StartGameDialog();
